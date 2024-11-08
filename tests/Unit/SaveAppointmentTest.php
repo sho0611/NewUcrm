96763992be844b79cb52a9b1dcc13a543a64d1ce
Showaa-06 <shogoshogo0611@icloud.com>
2024-11-08 15:39:13 +0900

    a

--- tests/Unit/SaveAppointmentTest.php
+++ tests/Unit/SaveAppointmentTest.php
@@ -12,42 +12,42 @@ use App\Data\AppointmentResult;
 
 class SaveAppointmentTest extends TestCase
 {
-    // protected function tearDown(): void
-    // {
-    //     Mockery::close();
-    //     parent::tearDown();
-    // }
+    protected function tearDown(): void
+    {
+        Mockery::close();
+        parent::tearDown();
+    }
 
-    // public function testSaveAppointments()
-    // {
-    //     // テスト用の AppointmentData を作成
-    //     $appointmentData = new AppointmentData(
-    //         itemIds: [3, 2],
-    //         customerId: 1,
-    //         staffId: 2,
-    //         appointmentDate: '2031-03-26',
-    //         appointmentTime: '14:22'
-    //     );
+    public function testSaveAppointments()
+    {
+        // テスト用の AppointmentData を作成
+        $appointmentData = new AppointmentData(
+            itemIds: [3, 2],
+            customerId: 1,
+            staffId: 2,
+            appointmentDate: '2031-03-26',
+            appointmentTime: '14:22'
+        );
 
-    //     // AppointmentSaverInterface のモックを作成
-    //     $appointmentSaverMock = Mockery::mock(AppointmentSaverInterface::class);
+        // AppointmentSaverInterface のモックを作成
+        $appointmentSaverMock = Mockery::mock(AppointmentSaverInterface::class);
 
-    //     // saveAppointments メソッドが期待する値を返すように設定
-    //     $appointmentSaverMock->shouldReceive('saveAppointments')
-    //                     ->once()
-    //                     ->with($appointmentData)
-    //                     ->andReturn(new AppointmentResult([]));
+        // saveAppointments メソッドが期待する値を返すように設定
+        $appointmentSaverMock->shouldReceive('saveAppointments')
+                        ->once()
+                        ->with($appointmentData)
+                        ->andReturn(new AppointmentResult([]));
 
         
-    //     //モックを使って SaveAppointment インスタンスを作成
-    //     $saveAppointment = new SaveAppointment($appointmentSaverMock);
+        //モックを使って SaveAppointment インスタンスを作成
+        $saveAppointment = new SaveAppointment($appointmentSaverMock);
 
-    //     // saveAppointments メソッドを実行し、結果を検証
-    //     $result = $saveAppointment->saveAppointments($appointmentData);
+        // saveAppointments メソッドを実行し、結果を検証
+        $result = $saveAppointment->saveAppointments($appointmentData);
 
-    //     // 返された結果が AppointmentResult インスタンスであることを確認
-    //     $this->assertInstanceOf(AppointmentResult::class, $result);
-    // }
+        // 返された結果が AppointmentResult インスタンスであることを確認
+        $this->assertInstanceOf(AppointmentResult::class, $result);
+    }
 }
 
 
